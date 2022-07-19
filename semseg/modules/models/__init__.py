24424b019b357dd5459b5cfe11abcb147b8b5d18
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2022-07-19 23:11:58 +0700

    FCN for semantic segmentation

---
+++ semseg/modules/models/__init__.py
@@ -0,0 +1,25 @@
+from semseg.modules.models.fcn import FCN8s
+import copy
+import torchvision.models as models
+
+
+def get_model(model_dict, n_classes, version=None):
+    name = model_dict['arch']
+    model = _get_model_instance(name)
+    param_dict = copy.deepcopy(model_dict)
+    param_dict.pop("arch")
+
+    if name in ["fcn8s"]:
+        model = model(n_classes=n_classes, **param_dict)
+        vgg16 = models.vgg16(pretrained=True)
+        model.init_vgg16_params(vgg16)
+    return model
+
+
+def _get_model_instance(name):
+    try:
+        return {
+            "fcn8s": FCN8s
+        }[name]
+    except:
+        raise ("Model {} not available".format(name))
